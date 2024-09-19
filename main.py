a708499f2bb5d72e3494224f273471a808642897
yusuf özil <yusuf_ozil_55@hotmail.com>
2024-09-19 16:08:35 +0300

    make an error

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def isOdd(number):
-    if type(number) != int:
-        return "error"
+    # if type(number) != int:
+    #     return "error"
     if number % 2 == 0:
         return False
     else:
